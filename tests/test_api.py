69d1b3395bd054767dcfb0d914e5ccef2ce90480
aaravgotra <aarav.gotra5@gmail.com>
2025-08-07 23:00:32 +0530

    part 4 and part 5 completed

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,6 +6,7 @@ from api.main import app
 client = TestClient(app)
 
 def test_predict_endpoint():
+    print(fastapi.testclient.__file__)
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to the Iris Prediction API"}
