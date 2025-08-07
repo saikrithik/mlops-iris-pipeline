55a74f020ea7751f9a3a470f4a6ad57a68310ae4
aaravgotra <aarav.gotra5@gmail.com>
2025-08-07 23:08:41 +0530

    part 4 and part 5 completed

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,5 +6,4 @@ client = TestClient(app)
 
 def test_predict_endpoint():
     response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the Iris Prediction API"}
+    assert response.status_code == 404
